050cdbe891354a6243b4a80232adf16f41f56be9
Anderson Luan <anderson@Andersons-MacBook-Pro.local>
2022-11-18 16:44:14 -0500

    Create new files and update vue.js

---
+++ my-app/src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>    
+        <h1>Hi There!</h1>
+        <body>
+            <div>
+                Welcome to my website. 
+                Sadly there isn't much here at the moment since it is still under construction 
+                but do take a look around.
+            </div>
+        </body>
+    </div>
+</template>
