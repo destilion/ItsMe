bbce62c85c02319f693dcc2a2e4dc480a922f02a
Anderson Luan <anderson@Andersons-MacBook-Pro.local>
2022-12-04 22:05:21 -0500

    remove submodule

---
+++ my-site/src/App.vue
@@ -0,0 +1,63 @@
+<script>
+import Home from './components/HomeComponent.vue'
+import Resume from './components/ResumeComponent.vue'
+import Projects from './components/ProjectsComponent.vue'
+
+const routes = {
+  '/': Home,
+  '/resume': Resume,
+  '/projects': Projects
+}
+
+export default {
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1)||'/']
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+      this.currentPath = window.location.hash
+		})
+  }
+}
+</script>
+<template>
+  <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <button><a href="/">Home</a></button>
+          <button><a href="#/resume">Resume</a></button>
+          <button><a href="#/projects">Projects</a></button>
+        </div>
+        <div class="col-md-4">
+          <button>Switch</button>
+        </div>
+      </div>
+      <div>
+        <component :is="currentView" />
+      </div>
+  </div>
+</template>
+
+<template>
+  <div id="app">
+    
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ my-site/src/components/HomeComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home">    
+        <h1>Hi There!</h1>
+        <body>
+            <div>
+                Welcome to my website. 
+                Sadly there isn't much here at the moment since it is still under construction 
+                but do take a look around.
+            </div>
+        </body>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'home',
+    data () {
+        return {
+            msg: 'Hello message'
+        }
+    }
+}
+</script>  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
