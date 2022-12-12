9462e88d250cecc87c5c2d35a0bf31f464c67ccc
destilion <destilion@users.noreply.github.com>
2022-12-12 15:04:21 -0500

    fix grammar and typos

--- my-site/src/components/ResumeComponent.vue
+++ my-site/src/components/ResumeComponent.vue
@@ -1,18 +1,31 @@
 <template>
-    <div style="position: top; height:100vh">
-        <div class="first-row">
-            <h1>Resume</h1>
-        </div>
+  <div class="resumee">
+    <div class="first-row">
+      <h1>Resume</h1>
+    </div>
 
-        <embed class="resume container-md" src="https://drive.google.com/file/d/1CXiO2iqh4V9YAsn10LzIaZsQiSy-Pq4H/preview" style="height: -webkit-fill-available">
+    <embed class="resume container-md" src="https://drive.google.com/file/d/1CXiO2iqh4V9YAsn10LzIaZsQiSy-Pq4H/preview"
+      style="height: -webkit-fill-available">
 
-        </div>
+  </div>
+  <div class="footer">
+
+  </div>
 </template>
 <script>
-    export default {
-        name: 'resume-component'
-    }
+export default {
+  name: 'resume-component'
+}
 </script>
 <style scoped>
-
+  .footer {
+  padding-bottom: 15vh;
+  }
+  .resumee{
+    height: 85vh;
+  }
+  .resume{
+    max-height:95vh;
+    height: 5vh;
+  }
 </style>
